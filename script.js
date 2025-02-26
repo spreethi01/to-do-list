92aca364bdd3807843fff6c9027a2229eeaf8730
spreethi01 <preethi.official098@gmail.com>
2025-02-26 19:02:43 +0530

    to do list

--- script.js
+++ script.js
@@ -11,16 +11,20 @@ function addTask() {
         li.innerHTML = inputBox.value;
         listContainer.appendChild(li);
         let span = document.createElement("span");
-        span.innerHtTML="\u00d7";
+        span.innerHTML="\u00d7";
+        li.appendChild(span);
     }
     inputBox.value="";
+    saveData();
 }
 listContainer.addEventListener("click", function(e) {
     if (e.target.tagName === "LI") {
-        e.target.classList.toggle("checked");
+        e.target.classList.toggle("check");
+        saveData();
     } 
     else if (e.target.tagName === "SPAN") {
         e.target.parentElement.remove();
+        saveData();
     }
 }, false);
 function saveData(){
